99995ab330e37202ddf4c9da6989ef49a325c61b
Zdenek Tichý <tichy@lib.cas.cz>
2026-09-04 12:27:54 +0200

    Update get_token-test.py

--- Scripts/get_token-test.py
+++ Scripts/get_token-test.py
@@ -7,7 +7,7 @@ import jwt # pip install pyjwt
 app_id = 'aba007'
 
 
-shared_secret = '790f7076-bfe1-4eeb-88a7-c0f6e736253c' #test
+shared_secret = 'xxxxx' #test
 
 now = datetime.datetime.utcnow()
 
